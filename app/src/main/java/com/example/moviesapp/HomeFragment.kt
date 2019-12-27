038d5798418e52f8c63cca944fb67dcb1dd7fa81
HassanSobhy <hassansobhy60@gmail.com>
2019-12-27 02:11:51 +0200

    Add home fragment ui

--- app/src/main/java/com/example/moviesapp/HomeFragment.kt
+++ app/src/main/java/com/example/moviesapp/HomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.moviesapp.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -14,10 +15,15 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val binding = FragmentHomeBinding.inflate(inflater)
+
+
+
+
+
+        return binding.root
     }
 
 
